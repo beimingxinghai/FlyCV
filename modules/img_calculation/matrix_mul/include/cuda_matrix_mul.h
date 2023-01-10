7933e4d1c105707242fe84de84003299b644e6cf
hcms1994 <1719468515@qq.com>
2023-01-10 15:26:17 +0800

    Add matmul cuda implementation

---
+++ modules/img_calculation/matrix_mul/include/cuda_matrix_mul.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 FlyCV Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "modules/core/mat/interface/cuda_mat.h"
+
+G_FCV_NAMESPACE1_BEGIN(g_fcv_ns)
+
+/**
+ * @brief the implementation of perspective matrix_mul_cuda, supported element types: int, f32, double
+ * The method returns a temporary object encoding per-element array multiplication,
+   Computed with OpenCL
+ * @param dst dst image, supported image type:Mat,the number of channel: 1 
+ * @param src0 source image, supported image type:Mat, the number of channel: 1.
+ * @param src1 another source image, supported image type:Mat, the number of channel: 1.
+ */
+int cuda_matrix_mul(CudaMat& dst, const CudaMat& src0, const CudaMat& src1);
+
+G_FCV_NAMESPACE1_END()
---
+++ modules/img_calculation/matrix_mul/src/cuda_matrix_mul.cu
@@ -0,0 +1,128 @@
+// Copyright (c) 2022 FlyCV Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "modules/core/base/include/type_info.h"
+#include "modules/img_calculation/matrix_mul/include/cuda_matrix_mul.h"
+
+G_FCV_NAMESPACE1_BEGIN(g_fcv_ns)
+
+#define BLOCK_SIZE 32
+
+template<typename T>
+__global__ void matrix_mul_c1(T* dst, T* src0, T* src1, int w, int h, int k) {
+    __shared__ T tile_0[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ T tile_1[BLOCK_SIZE][BLOCK_SIZE];
+
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
+    int ty = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int N = (k + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    T data = static_cast<T>(0);
+    for (int i = 0; i < N; i++) {
+        int row0 = ty * k + i * BLOCK_SIZE + threadIdx.x;
+        int row1 = k * (i * BLOCK_SIZE + threadIdx.y) + tx;
+        if (tx < w && ty < h) {
+            tile_0[threadIdx.y][threadIdx.x] = src0[row0];
+            tile_1[threadIdx.y][threadIdx.x] = src1[row1];
+            
+            __syncthreads();
+
+            for (int j = 0; j < BLOCK_SIZE; j++) {
+                data += tile_0[threadIdx.y][j] * tile_1[j][threadIdx.x];
+            }
+        }
+    }
+    if (tx < w && ty < h) {
+        dst[ty * w + tx] = data;
+    }
+}
+
+int cuda_matrix_mul(CudaMat& dst, const CudaMat& src0, const CudaMat& src1) {
+    if (src0.empty() || src1.empty()) {
+        LOG_ERR("The input data is empty!");
+        return -1;
+    }
+    if (src0.width() != src1.height()) {
+        LOG_ERR("The width of src0 should be the same with the height of src1!");
+        return -1;
+    }
+
+    if (src0.channels() != src1.channels()
+             || src0.type() != src1.type()) {
+        LOG_ERR("The channel and data type of matrix_mul should be the same!");
+        return -1;
+    }
+
+    if (dst.empty()) {
+        dst = CudaMat(src1.width(), src0.height(), src0.type());
+    }
+
+    TypeInfo type_info;
+    int status = get_type_info(src0.type(), type_info);
+    size_t src0_size = src0.height() * src0.width() * type_info.type_byte_size;
+    size_t src1_size = src1.height() * src1.width() * type_info.type_byte_size;
+    size_t dst_size = src1.width() * src0.height() * type_info.type_byte_size;
+
+    const int dst_c = dst.channels();
+    const int dst_w = src1.width();
+    const int dst_h = src0.height();
+    const int mat_k = src0.width();
+
+    const int grid_x_size = (dst_w + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    const int grid_y_size = (dst_h + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    const dim3 blocksize(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 gridsize(grid_y_size, grid_x_size);
+
+    int device_id = 0;
+    CUDA_CHECK(cudaGetDevice(&device_id));
+    int coherent_flag = CUDADeviceInfo::get_instance()->device_attrs[device_id].coherent_flag;
+    if (coherent_flag) {
+        CUDA_CHECK(cudaMemPrefetchAsync(dst.data(), dst_size, device_id));
+        CUDA_CHECK(cudaMemPrefetchAsync(src0.data(), src0_size, device_id));
+        CUDA_CHECK(cudaMemPrefetchAsync(src1.data(), src1_size, device_id));
+    }
+
+    if (dst_c == 1) {
+        switch(type_info.data_type) {
+            case DataType::UINT16:
+                matrix_mul_c1<unsigned short><<<gridsize, blocksize>>>((unsigned short*)dst.data(), 
+                    (unsigned short*)src0.data(), (unsigned short*)src1.data(), dst_w, dst_h, mat_k);
+                break;
+            case DataType::SINT32:
+                matrix_mul_c1<int><<<gridsize, blocksize>>>((int*)dst.data(), (int*)src0.data(), (int*)src1.data(), dst_w, dst_h, mat_k);
+                break;
+            case DataType::F32:
+                matrix_mul_c1<float><<<gridsize, blocksize>>>((float*)dst.data(), (float*)src0.data(), (float*)src1.data(), dst_w, dst_h, mat_k);
+                break;
+            case DataType::F64:
+                matrix_mul_c1<double><<<gridsize, blocksize>>>((double*)dst.data(), (double*)src0.data(), (double*)src1.data(), dst_w, dst_h, mat_k);
+                break;
+            default:
+                LOG_ERR("The src type is not supported!");
+                break;
+        }
+    } else {
+        LOG_ERR("The src channel is not supported!");
+        return -1;
+    }
+
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
+
+    return 0;
+}
+
+G_FCV_NAMESPACE1_END()
